c10277e3d19a2d47a24f2f346afc3d533a92ffdc
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-12-09 21:07:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-1 from string_utils import split_by_capitals, split_at_number, parse_chemical_reaction, count_atoms_in_molecule, count_atoms_in_reaction
-2 from equation_utils import ELEMENTS, generate_equation_for_element, build_equations, my_solve
-3 from sympy import Eq, symbols, solve
-4
-5 def balance_reaction(reaction): # "Fe2O3 + H2 -> Fe + H2O"
-6
-7     # 1.parse reaction
-8     reactants, products = parse_chemical_reaction(reaction) # ["Fe2O3", "H2"], ["Fe", "H2O"]
-9     reactant_atoms = count_atoms_in_reaction(reactants) # [{'Fe':2, 'O':1}, {'H':2}]
-10    product_atoms = count_atoms_in_reaction(products)
-11
-12    # 2.build equation and solve
-13    equations, coefficients = build_equations(reactant_atoms, product_atoms)
-14    coefficients = my_solve(equations, coefficients) + [1]
-15
-16    return coefficients # [1/3, 1, 2/3, 1]
+from equation_utils import ELEMENTS,generate_equation_for_element,build_equations,my_solve
+from string_utils import split_before_uppercases,split_at_digit,count_atoms_in_molecule,parse_chemical_reaction,count_atoms_in_reaction
+from sympy import Eq,symbols,solve
+
+
+def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
+
+    # 1.parse reaction
+    reactants, products = parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
+    reactant_atoms = count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
+    product_atoms = count_atoms_in_reaction(products)
+
+    # 2.build equation and solve
+    equations, coefficients = build_equations(reactant_atoms, product_atoms)
+    coefficients = my_solve(equations, coefficients) + [1]
+
+    return coefficients # [1/3, 1, 2/3, 1]
